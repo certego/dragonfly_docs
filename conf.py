66d3ced7cd55dd86c94c897458e8b7b3d610004c
0ssigeno <simone.berni2@studio.unibo.it>
2021-06-14 10:38:59 +0200

    Excluding readme

--- conf.py
+++ conf.py
@@ -24,7 +24,6 @@ author = 'Certego'
 # The full version, including alpha/beta/rc tags
 release = '1.2.5'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -42,8 +41,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "README.md"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -56,3 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+source_suffix = {
+    ".rst": "restructuredtext"
+}
