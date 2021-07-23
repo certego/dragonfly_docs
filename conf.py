70baedafc7262ee2d400cfbaafe027643020d708
0ssigeno <simone.berni2@studio.unibo.it>
2021-07-23 11:11:36 +0200

    Black configuration

--- conf.py
+++ conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Dragonfly'
-copyright = '2021, Certego'
-author = 'Certego'
+project = "Dragonfly"
+copyright = "2021, Certego"
+author = "Certego"
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.5'
+release = "1.2.5"
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,33 +32,30 @@ release = '1.2.5'
 extensions = [
     "recommonmark",
     "sphinx_rtd_theme",
-
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "README.md"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    'sticky_navigation': True,
-    'navigation_depth': 6,
-    }
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-source_suffix = {
-    ".rst": "restructuredtext"
-}
+source_suffix = {".rst": "restructuredtext"}
